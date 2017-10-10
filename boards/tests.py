15c04d184f579c5070568ac6836b15433a3330d4
lbias <306508354@qq.com>
2017-10-10 16:47:41 +0800

    test board topics view contains naviagation links

--- boards/tests.py
+++ boards/tests.py
@@ -40,11 +40,13 @@ class BoardTopicsTests(TestCase):
         view = resolve('/boards/1/')
         self.assertEquals(view.func, board_topics)
 
-    def test_board_topics_view_contains_link_back_to_homepage(self):
+    def test_board_topics_view_contains_navigation_links(self):
         board_topics_url = reverse('board_topics', kwargs={'pk': 1})
-        response = self.client.get(board_topics_url)
         homepage_url = reverse('home')
+        new_topic_url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.get(board_topics_url)
         self.assertContains(response, 'href="{0}"'.format(homepage_url))
+        self.assertContains(response, 'href="{0}"'.format(new_topic_url))
 
 class NewTopicTests(TestCase):
     def setUp(self):
