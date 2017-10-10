a6f2fae76e862dd542daf709d1f84e91197b0d6a
lbias <306508354@qq.com>
2017-10-10 13:58:14 +0800

    view board_topics

--- boards/views.py
+++ boards/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 
 from .models import Board
 
@@ -7,5 +8,8 @@ def home(request):
     return render(request, 'home.html', {'boards':boards})
 
 def board_topics(request, pk):
-    board = Board.objects.get(pk=pk)
+    try:
+        board = Board.objects.get(pk=pk)
+    except Board.DoesNotExist:
+        raise Http404
     return render(request, 'topics.html', {'board': board})
