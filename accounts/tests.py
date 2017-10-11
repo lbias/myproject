abd9cd654bc9ceaea92fa5660243c02bc5677bd6
lbias <306508354@qq.com>
2017-10-11 10:41:46 +0800

    test if there is a form and the csrf token in the response

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.urls import resolve
@@ -5,11 +6,20 @@ from django.urls import resolve
 from .views import signup
 
 class SignUpTests(TestCase):
-    def test_signup_status_code(self):
+    def setUp(self):
         url = reverse('signup')
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+        self.response = self.client.get(url)
+
+    def test_signup_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
 
     def test_signup_url_resolves_signup_view(self):
         view = resolve('/signup/')
         self.assertEquals(view.func, signup)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_contains_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, UserCreationForm)
