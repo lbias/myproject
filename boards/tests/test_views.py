97a9d1c90397f1c0b2ee9753575e15a7c74c8198
lbias <306508354@qq.com>
2017-10-11 18:17:11 +0800

    fix test_views.py imports

--- boards/tests/test_views.py
+++ boards/tests/test_views.py
@@ -3,9 +3,9 @@ from django.core.urlresolvers import reverse
 from django.urls import resolve
 from django.test import TestCase
 
-from .forms import NewTopicForm
-from .models import Board, Topic, Post
-from .views import home, board_topics, new_topic
+from ..forms import NewTopicForm
+from ..models import Board, Topic, Post
+from ..views import home, board_topics, new_topic
 
 class HomeTests(TestCase):
     def setUp(self):
