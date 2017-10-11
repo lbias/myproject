05a04a33462f56e70bd5a4f849c0d3c8f8e3f439
lbias <306508354@qq.com>
2017-10-11 19:25:24 +0800

    basic setup for test view password change

--- accounts/tests/test_view_password_change.py
+++ accounts/tests/test_view_password_change.py
@@ -11,3 +11,14 @@ class LoginRequiredPasswordChangeTests(TestCase):
         login_url = reverse('login')
         response = self.client.get(url)
         self.assertRedirects(response, f'{login_url}?next={url}')
+
+class PasswordChangeTestCase(TestCase):
+    '''
+    Base test case for form processing
+    accepts a `data` dict to POST to the view.
+    '''
+    def setUp(self, data={}):
+        self.user = User.objects.create_user(username='john', email='john@doe.com', password='old_password')
+        self.url = reverse('password_change')
+        self.client.login(username='john', password='old_password')
+        self.response = self.client.post(self.url, data)
