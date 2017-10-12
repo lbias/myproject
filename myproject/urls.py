b54832df38d38ca8a81bd71c278aba9943dd1437
lbias <306508354@qq.com>
2017-10-12 22:07:25 +0800

    pagination using boards#TopicListView

--- myproject/urls.py
+++ myproject/urls.py
@@ -23,7 +23,7 @@ from boards import views
 urlpatterns = [
     url(r'^$', views.BoardListView.as_view(), name='home'),
     url(r'^admin/', admin.site.urls),
-    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^boards/(?P<pk>\d+)/$', views.TopicListView.as_view(), name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$', views.topic_posts, name='topic_posts'),
     url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/reply/$', views.reply_topic, name='reply_topic'),
