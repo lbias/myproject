4f2d03d2814870221c58096510bfb816d3834ba9
lbias <306508354@qq.com>
2017-10-13 12:08:24 +0800

    use markdown in the post message

--- boards/models.py
+++ boards/models.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.html import mark_safe
 from django.utils.text import Truncator
+
+from markdown import markdown
 
 class Board(models.Model):
     name = models.CharField(max_length=30, unique=True)
@@ -36,3 +39,6 @@ class Post(models.Model):
     def __str__(self):
         truncated_message = Truncator(self.message)
         return truncated_message.chars(30)
+
+    def get_message_as_markdown(self):
+        return mark_safe(markdown(self.message, safe_mode='escape'))
