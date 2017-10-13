536c25578b33f848ac2a35ee69d2867a19fc4a49
lbias <306508354@qq.com>
2017-10-13 11:48:05 +0800

    TopicPostsTests#test_view_function

--- boards/tests/test_view_topic_posts.py
+++ boards/tests/test_view_topic_posts.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.urls import resolve, reverse
 
 from ..models import Board, Post, Topic
-from ..views import topic_posts
+from ..views import PostListView
 
 class TopicPostsTests(TestCase):
     def setUp(self):
@@ -19,4 +19,4 @@ class TopicPostsTests(TestCase):
 
     def test_view_function(self):
         view = resolve('/boards/1/topics/1/')
-        self.assertEquals(view.func, topic_posts)
+        self.assertEquals(view.func.view_class, PostListView)
