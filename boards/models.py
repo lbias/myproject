6708ae2ee4d7a989d81c52f5af51aebb79aa7cbb
lbias <306508354@qq.com>
2017-10-13 14:15:00 +0800

    tiny pagination for each topic

--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,5 @@
+import math
+
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils.html import mark_safe
@@ -27,6 +29,22 @@ class Topic(models.Model):
 
     def __str__(self):
         return self.subject
+
+    def get_page_count(self):
+        count = self.posts.count()
+        pages = count / 10
+        return math.ceil(pages)
+
+    def has_many_pages(self, count=None):
+        if count is None:
+            count = self.get_page_count()
+        return count > 6
+
+    def get_page_range(self):
+        count = self.get_page_count()
+        if self.has_many_pages(count):
+            return range(1, 5)
+        return range(1, count + 1)
 
 class Post(models.Model):
     message = models.TextField(max_length=4000)
